1d17b30f83641ccaf94e3d0d27a47eb776c64524
b.palowski <brianpalowski00@gmail.com>
2019-02-19 18:08:25 -0800

    Updated name validation

---
+++ app.js
@@ -0,0 +1,31 @@
+document.getElementById('name').addEventListener('blur', validateName);
+
+document.getElementById('zip').addEventListener('blur', validateZip);
+
+document.getElementById('email').addEventListener('blur', validateEmail);
+
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
+function validateName() {
+	const name = document.getElementById('name');
+	const reg = /^[a-zA-Z]{2,10}$/;
+
+	if (!reg.test(name.value)) {
+		name.classList.add('is-invalid');
+	} else {
+		name.classList.remove('is-invalid');
+	}
+}
+
+function validateZip() {
+
+}
+
+function validateEmail() {
+
+}
+
+function validatePhone() {
+
+}
