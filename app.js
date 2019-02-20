4aa30973bfa0f5fc10f81c19cb81b873664ce96b
b.palowski <brianpalowski00@gmail.com>
2019-02-19 18:23:24 -0800

    Updated phone validation

--- app.js
+++ app.js
@@ -19,13 +19,34 @@ function validateName() {
 }
 
 function validateZip() {
+	const zip = document.getElementById('zip');
+	const reg = /^[0-9]{5}(-[0-9]{4})?$/;
 
+	if (!reg.test(zip.value)) {
+		zip.classList.add('is-invalid');
+	} else {
+		zip.classList.remove('is-invalid');
+	}
 }
 
 function validateEmail() {
+	const email = document.getElementById('email');
+	const reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
+	if (!reg.test(email.value)) {
+		email.classList.add('is-invalid');
+	} else {
+		email.classList.remove('is-invalid');
+	}
 }
 
 function validatePhone() {
+	const phone = document.getElementById('phone');
+	const reg = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
 
+	if (!reg.test(phone.value)) {
+		phone.classList.add('is-invalid');
+	} else {
+		phone.classList.remove('is-invalid');
+	}
 }
